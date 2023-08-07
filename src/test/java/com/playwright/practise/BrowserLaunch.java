11ad0a34782369215016fbcddde081118ad4d62a
mgovindm5016 <mgovindarajm@gmail.com>
2023-08-07 16:09:38 +0530

    Pusing local repo

---
+++ src/test/java/com/playwright/practise/BrowserLaunch.java
@@ -0,0 +1,29 @@
+package com.playwright.practise;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+
+public class BrowserLaunch {
+
+	public static void main(String[] args) {
+		
+		Playwright playwright=Playwright.create();
+		//Browser browser=playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+		//Browser browser=playwright.firefox().launch(new BrowserType.LaunchOptions().setHeadless(false));
+		Browser browser=playwright.webkit().launch(new BrowserType.LaunchOptions().setHeadless(false));
+
+		Page page=browser.newPage();
+		page.navigate("https://www.amazon.com");
+		
+		System.out.println("Page title :"+ page.title());
+		System.out.println("Page Url:"+ page.url());
+		
+		browser.close();
+		playwright.close();
+		
+
+	}
+
+}
---
+++ src/test/java/com/playwright/practise/BrowserSpecfic.java
@@ -0,0 +1,33 @@
+package com.playwright.practise;
+
+import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType.LaunchOptions;
+import com.microsoft.playwright.Page;
+
+public class BrowserSpecfic {
+	public static void main(String[] args) {
+		
+		Playwright playwright=Playwright.create();
+		
+		LaunchOptions lp=new LaunchOptions();
+	      // Channel can be "chrome", "msedge", "chrome-beta", "msedge-beta" or "msedge-dev".
+		//lp.setChannel("msedge");
+		lp.setChannel("firefox");
+		lp.setHeadless(false);
+		
+		Browser browser=playwright.firefox().launch(lp);
+		
+		Page page=browser.newPage();
+		
+		page.navigate("https://www.amazon.com");
+		
+		System.out.println("Get Title :"+ page.title());
+		System.out.println("Get Url :"+ page.url());
+		
+		browser.close();
+		
+		playwright.close();
+
+	}
+}
